603b3835b2958e2babbc66f289616d1e381a86a3
Aleksey Karasev <karasev.dev@gmail.com>
2018-09-28 20:01:51 +0300

    update auth, hide button onclick

--- src/assets/scripts/auth.js
+++ src/assets/scripts/auth.js
@@ -1,19 +1,23 @@
 const front = document.querySelector('.welcome__front');
-const btns = document.querySelectorAll('.js-auth-btn');
+const authBtn = document.querySelector('.js-auth-btn');
+const mainBtn = document.querySelector('.js-main-btn');
 const flipper = document.querySelector('.welcome__flipper');
 
-for (let i = 0; i < btns.length; i++) {
-  const btn = btns[i];
-  btn.addEventListener('click', () => {
-    if (front.classList.contains('js-flip')) {
-      flipper.style.transform = "rotateY(180deg)";
-      front.classList.remove('js-flip')
-    } else {
-      flipper.style.transform = "rotateY(0deg)";
-      front.classList.add('js-flip')
-    }
-  })
-}
+
+authBtn.onclick = function () {
+  if (front.classList.contains('js-flip')) {
+    flipper.style.transform = "rotateY(180deg)";
+    front.classList.remove('js-flip');
+    authBtn.style.display = 'none'
+  }
+};
+mainBtn.onclick = function () {
+  flipper.style.transform = "rotateY(0deg)";
+  front.classList.add('js-flip')
+  authBtn.style.display = 'block'
+};
+
+
 
 
 
