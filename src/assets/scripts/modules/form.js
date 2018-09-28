a47127d309d5862dcbcdf242869ec3c1facd46a5
Aleksey Karasev <karasev.dev@gmail.com>
2018-09-28 19:35:40 +0300

    add blur effects

--- src/assets/scripts/modules/form.js
+++ src/assets/scripts/modules/form.js
@@ -2,11 +2,15 @@ const blur = (function () {
   const container = document.querySelector('.js-blur-wrapper');
   const formBlur = document.querySelector('.js-blur');
 
+
   return {
     set: function() {
       const bgWidth = document.querySelector('.js-reviews-bg').offsetWidth;
+      const bgHeight = document.querySelector('.js-reviews-bg').offsetHeight;
+      const sectionHeight = document.querySelector('.reviews').offsetHeight;
+      const differenceHeight = sectionHeight - bgHeight;
       const posLeft = -container.offsetLeft;
-      const posTop = -container.offsetTop;
+      const posTop = - container.offsetTop + differenceHeight;
       const blurCSS = formBlur.style;
 
       blurCSS.backgroundPosition = posLeft + 'px' + ' ' + posTop + 'px';
