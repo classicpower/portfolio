7c55ff567d174ed4797a6890ef37e3690da7c241
Aleksey Karasev <karasev.dev@gmail.com>
2018-09-30 17:39:30 +0300

    end js blog menu

--- src/assets/scripts/modules/blog-menu.js
+++ src/assets/scripts/modules/blog-menu.js
@@ -1,14 +1,27 @@
 const asideBtn = document.querySelector('.js-aside-btn');
 const aside = document.querySelector('.js-aside');
 
-asideBtn.addEventListener("click", function(e) {
-  e.preventDefault();
-  let asideWidth = aside.clientWidth;
-  if (!this.classList.contains('js-aside-open')) {
-    this.classList.add('js-aside-open');
-    aside.style.left = 0;
-  }else{
-    this.classList.remove('js-aside-open');
+function asideWidth() {
+
+    let asideWidth = aside.clientWidth;
     aside.style.left = -asideWidth + 'px';
+
+
+}
+function openMenu() {
+  if (!asideBtn.classList.contains('js-aside-open')) {
+    asideBtn.classList.add('js-aside-open');
+    aside.style.left = 0;
+  } else {
+    asideBtn.classList.remove('js-aside-open');
+    asideWidth()
   }
-})
+}
+
+asideBtn.onclick = function () {
+  openMenu();
+};
+window.onresize = function () {
+  asideWidth();
+  asideBtn.classList.remove('js-aside-open');
+};
