d4de38e023907016993dbf78b249b48fff3994fa
Aleksey Karasev <karasev.dev@gmail.com>
2018-10-01 21:48:08 +0300

    edit js blog menu

--- src/assets/scripts/modules/blog-menu.js
+++ src/assets/scripts/modules/blog-menu.js
@@ -2,8 +2,8 @@ const asideBtn = document.querySelector('.js-aside-btn');
 const aside = document.querySelector('.js-aside');
 
 function asideWidth() {
-    let asideWidth = aside.clientWidth;
-    aside.style.left = -asideWidth + 'px';
+  let asideWidth = aside.clientWidth;
+  aside.style.left = -asideWidth + 'px';
 }
 function openMenu() {
   if (!asideBtn.classList.contains('js-aside-open')) {
@@ -14,6 +14,20 @@ function openMenu() {
     asideWidth()
   }
 }
+function showMenu() {
+  const header = document.querySelector('.header');
+  let heightHeader = header.clientHeight;
+  let winWidth = window.screen.width;
+  if(window.scrollY > heightHeader / 2 && winWidth < 768){
+    asideBtn.style.display = "flex";
+  }
+  if(window.scrollY < heightHeader / 2){
+    asideBtn.style.display = "none";
+  }
+  if(winWidth > 768){
+    asideBtn.style.display = "none";
+  }
+}
 
 asideBtn.onclick = function () {
   openMenu();
@@ -21,9 +35,14 @@ asideBtn.onclick = function () {
 window.onresize = function () {
   asideWidth();
   asideBtn.classList.remove('js-aside-open');
+  showMenu();
 };
 window.onload = function () {
   asideWidth();
   asideBtn.classList.remove('js-aside-open');
+  showMenu();
+};
+window.onscroll = function () {
+  showMenu();
 };
 
