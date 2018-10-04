cbd12b20b1e0ee4eead7d43430d272905287a6c2
Aleksey Karasev <karasev.dev@gmail.com>
2018-10-04 16:39:39 +0300

    add script select active item menu blog

--- src/assets/scripts/blog.js
+++ src/assets/scripts/blog.js
@@ -1,3 +1,28 @@
 import "./modules/hamburger";
 import "./modules/footer";
 import "./modules/blog-menu";
+
+const items = document.querySelectorAll('.blog-menu__item');
+const itemActive = document.querySelector('.blog-menu__item--active');
+const btns = document.querySelectorAll('.blog-menu__btn');
+
+
+for (let i = 0; i < btns.length; i++) {
+  const button = btns[i];
+  button.onclick = function () {
+    let parent = this.parentNode;
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      item.classList.remove("blog-menu__item--active");
+    }
+    parent.classList.add("blog-menu__item--active");
+  }
+}
+
+// let nextParent = parent.nextElementSibling;
+// let prevParent = parent.previousElementSibling;
+// console.log(parent);
+// console.log(nextParent);
+// console.log(prevParent);
+// nextParent.classList.remove("blog-menu__item--active");
+// prevParent.classList.remove("blog-menu__item--active");
