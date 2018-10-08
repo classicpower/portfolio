eb2414e5e99cf221d9aa798266db1f396785edf1
Aleksey Karasev <karasev.dev@gmail.com>
2018-10-08 11:47:48 +0300

    update blur effect this background

--- src/assets/scripts/modules/form.js
+++ src/assets/scripts/modules/form.js
@@ -2,39 +2,27 @@ const blur = (function () {
   const container = document.querySelector('.js-blur-wrapper');
   const formBlur = document.querySelector('.js-blur');
 
-
   return {
     set: function() {
       const bgWidth = document.querySelector('.js-reviews-bg').offsetWidth;
       const bgHeight = document.querySelector('.js-reviews-bg').offsetHeight;
-      const sectionHeight = document.querySelector('.reviews').offsetHeight;
-      const differenceHeight = sectionHeight - bgHeight;
       const posLeft = -container.offsetLeft;
-      const posTop = -container.offsetTop + differenceHeight;
+      const posTop = -container.offsetTop;
       const blurCSS = formBlur.style;
 
-      blurCSS.backgroundPosition = posLeft + 'px' + ' ' + posTop + 'px';
-      blurCSS.backgroundSize = bgWidth + 'px' + ' ' + 'auto';
+      blurCSS.width = `${bgWidth}px`;
+      blurCSS.height = `${bgHeight}px`;
+      blurCSS.transform = `translate(${posLeft}px,${posTop}px`;
+      blurCSS.webkitTransform = `translate(${posLeft}px,${posTop}px`;
+
 
     }
   }
 }());
 
-function heightForm() {
-  const form = document.getElementById('form');
-  const addHeight = document.querySelector('.js-add-height');
-  const addMargin = document.querySelector('.js-add-margin');
-  let height = form.clientHeight;
-  addHeight.style.height = height + 'px';
-  addMargin.style.marginTop = -height + 'px';
-}
-
-
 window.onresize = function() {
   blur.set();
-  heightForm()
 }
 window.onload = function() {
   blur.set();
-  heightForm()
 }
