bb804e051c657c38ff3582ad6e9dae1a4ee98112
Aleksey Karasev <karasev.dev@gmail.com>
2018-10-16 22:09:39 +0300

    edit bugs

--- src/assets/scripts/modules/form-login.js
+++ src/assets/scripts/modules/form-login.js
@@ -2,18 +2,22 @@ const form = document.getElementById('form');
 const submit = document.querySelector('.form__submit');
 const action = form.getAttribute('action');
 const method = form.getAttribute('method');
+const login = form.elements.login;
+const password = form.elements.password;
+const checkCaptcha = form.elements.checkCaptcha;
+
 
 submit.addEventListener("click", function (e) {
   e.preventDefault();
   if (validateForm(form)) {
     const data = {
-      name: form.elements.name.value,
+      login: form.elements.login.value,
       password: form.elements.password.value,
       checkCaptcha: form.elements.checkCaptcha.value,
-      radioCaptcha: form.elements.checkCaptcha.value,
+      radioCaptcha: form.elements.radioCaptcha.value,
       to: "karasev.dev@gmail.com"
     };
-
+    console.log(data);
     const xhr = new XMLHttpRequest();
     xhr.responseType = "json";
     xhr.open(method, action);
@@ -23,27 +27,80 @@ submit.addEventListener("click", function (e) {
 
   }
 });
+login.addEventListener('change', function () {
+  if (this.value.length <= 0) {
+    validateField(login, "Вы не ввели логин");
+  } else {
+    validateField(login, "");
+  }
+});
+password.addEventListener('change', function () {
+  if (this.value.length <= 3) {
+    validateField(password, "Пароль должен быть больше 3х символов");
+  } else {
+    validateField(password, "");
+  }
+});
+
 function validateForm(form) {
-  const name = form.elements.name;
+  const login = form.elements.login;
   const password = form.elements.password;
   const checkCaptcha = form.elements.checkCaptcha;
   const radioCaptcha = form.elements.radioCaptcha;
+  const check = document.querySelector('.form__checkbox-custom');
+  console.log(check);
   let valid = true;
-  if (!validateField(name)) {
-    valid = false;
+  if (login.value.length <= 0) {
+    validateField(login, "Вы не ввели логин");
+    valid = false
+  } else {
+    validateField(login, "");
+    valid = true
   }
-  if (!validateField(password)) {
-    valid = false;
+  if (password.value.length <= 3) {
+    validateField(password, "Пароль должен быть больше 3х символов");
+    valid = false
+  } else {
+    validateField(password, "")
+    valid = true
   }
-  if (!validateField(checkCaptcha)) {
+
+  if (!checkCaptcha.cheked) {
+    validateField(checkCaptcha, "Робот! Ты не пройдешь!");
     valid = false;
+  } else {
+    validateField(checkCaptcha, "");
+    valid = true;
   }
-  if (!validateField(radioCaptcha)) {
-    valid = false;
-  }
+
+  // if (!validateField(radioCaptcha)) {
+  //   valid = false;
+  // }
   return valid;
 }
-function validateField(field) {
-  field.nextElementSibling.textContent = field.validationMessage;
-  return field.checkValidity();
+function validateField(field, messege) {
+  const fieldError = field.nextElementSibling;
+  const parentLabel = field.parentNode;
+  if (field === login || field === password) {
+    fieldError.textContent = messege;
+    if (messege) {
+      const div = document.createElement('div');
+      div.className = 'form__error-triangle';
+      fieldError.appendChild(div);
+      parentLabel.classList.add('form__input-label--error');
+      parentLabel.classList.remove('form__input-label--success');
+    } else {
+      parentLabel.classList.add('form__input-label--success');
+      parentLabel.classList.remove('form__input-label--error');
+    }
+  }
+  if (field === checkCaptcha) {
+    const lastFieldError = parentLabel.children[3];
+    lastFieldError.textContent = messege;
+    if (messege) {
+      const div = document.createElement('div');
+      div.className = 'form__error-triangle';
+      lastFieldError.appendChild(div);
+    }
+  }
 }
