7eac5695526ea58cab69bcdb1f2bff121cdf49c9
Aleksey Karasev <karasev.dev@gmail.com>
2018-10-21 19:04:11 +0300

    edit bugs

--- src/admin/router.js
+++ src/admin/router.js
@@ -1,44 +1,44 @@
-// import Vue from "vue";
-// import VueRouter from "vue-router";
-// import axios from "axios";
+import Vue from "vue";
+import VueRouter from "vue-router";
+import axios from "axios";
 
-// Vue.use(VueRouter);
+Vue.use(VueRouter);
 
-// const guard = axios.create({
-//   baseURL: "http://localhost:8000"
-// });
+const guard = axios.create({
+  baseURL: "http://localhost:8000"
+});
 
-// import skills from './components/skills.vue';
+import skills from './components/skills.vue';
 // import works from './components/works.vue';
 
-// const routes = [
-//   {
-//     path: '/',
-//     component: skills
-//   },
-//   {
-//     path: '/works',
-//     component: works
-//   }
-// ];
+const routes = [
+  {
+    path: '/',
+    component: skills
+  },
+  // {
+  //   path: '/works',
+  //   component: works
+  // }
+];
 
-// const router = new VueRouter({ routes });
+const router = new VueRouter({ routes });
 
-// router.beforeEach((to, from, next) => {
-//   // guard
-//   //   .get("/user", {
-//   //     headers: {
-//   //       Authorization: `Bearer ${localStorage.getItem("token")}`
-//   //     }
-//   //   })
-//   //   .then(response => {
-//   //     // next();
-//   //   })
-//   //   .catch(error => {
-//   //     // localStorage.removeItem("token");
-//   //     // window.location.href = "http://google.com";
-//   //   });
-//   next();
-// });
+router.beforeEach((to, from, next) => {
+  guard
+    .get("/user", {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
+    // .then(response => {
+    //   next();
+    // })
+    // .catch(error => {
+    //   // localStorage.removeItem("token");
+    //   // window.location.href = "http://google.com";
+    // });
+  next();
+});
 
-// export default router;
+export default router;
