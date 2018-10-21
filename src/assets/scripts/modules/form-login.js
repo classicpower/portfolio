c50cb5af37449e55863971d5e120d075478a5a07
Aleksey Karasev <karasev.dev@gmail.com>
2018-10-21 16:45:55 +0300

    edit bugs

--- src/assets/scripts/modules/form-login.js
+++ src/assets/scripts/modules/form-login.js
@@ -19,8 +19,9 @@ submit.addEventListener("click", function (e) {
     const xhr = new XMLHttpRequest();
     xhr.responseType = "json";
     xhr.open(method, action);
+    console.log(method);
+    console.log(action);
     xhr.send(JSON.stringify(data));
-    console.log(JSON.stringify(data));
     xhr.addEventListener("load", function () {
       console.log(xhr.status);
       console.log(xhr.response);
